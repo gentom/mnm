43e51ce4cd3533b44887f5eb99ef34358129f52d
gentom <gento.m.developer@gmail.com>
2018-10-15 18:34:21 +0900

    init

---
+++ lib/mnm.ex
@@ -0,0 +1,18 @@
+defmodule Mnm do
+  @moduledoc """
+  Documentation for Mnm.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mnm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
