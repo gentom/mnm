89b002e43ef0350139feed025e4c1f255a683887
gentom <gento.m.developer@gmail.com>
2018-10-15 23:57:05 +0900

    commit

--- lib/mnm/application.ex
+++ lib/mnm/application.ex
@@ -1,8 +1,9 @@
 defmodule Mnm.Application do
   use Application
 
-  def start(_type, _args),
-    do: Supervisor.start_link(children(), opts())
+  def start(_type, _args) do
+    Supervisor.start_link(children(), opts())
+  end
 
   defp children do
     [
